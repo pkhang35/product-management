5b20cb504e7d270ed45dbfae0a6e20b91d46031c
pkhang35 <pkhang353@gmail.com>
2026-09-21 16:49:07 +0700

    Nhúng Bootstrap 4 - Nhúng file tĩnh

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const route = require('./routes/client/index.route')
 require('dotenv').config()
+
 const app = express();
 const port = process.env.PORT;
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+app.use(express.static('public'));
+
+//Routes
 route(app)
-// app.get('/', (req, res) => {
-//   res.render("client/pages/home/index");
-// });
-// app.get('/products', (req, res) => {
-//   res.render("client/pages/products/index")
-// });
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
