213a700ee04f4d8dcf15bc0aa57df528357ffb89
pkhang35 <pkhang353@gmail.com>
2026-09-22 08:20:18 +0700

    Cài đặt Mongoose

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const route = require('./routes/client/index.route')
+const database = require('./config/database')
 require('dotenv').config()
+const route = require('./routes/client/index.route')
 
 const app = express();
 const port = process.env.PORT;
+database.connect();
 
 app.set('views', './views')
 app.set('view engine', 'pug')
